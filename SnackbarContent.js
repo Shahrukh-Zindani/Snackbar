a10c2df3bb354373baa9200bcbe0287f4f7e4a01
Shahrukh-Zindani <shahrukh.zindani@gmail.com>
2017-03-13 15:09:48 -0500

    Snackbar initial commit

---
+++ SnackbarContent.js
@@ -0,0 +1,125 @@
+// @flow weak
+
+import React, { PropTypes } from 'react';
+import { createStyleSheet } from 'jss-theme-reactor';
+import customPropTypes from 'material-ui/utils/customPropTypes';
+import transitions from 'material-ui/styles/transitions';
+
+export const styleSheet = createStyleSheet('SnackbarContent', (theme) => {
+  const { typography } = theme;
+  return {
+    root: {
+      borderRadius: 2,
+      backgroundColor: 'rgb(50, 50, 50)',
+      padding: '0px 24px ',
+      lineHeight: '48px',
+    },
+    content: {
+      fontSize: typography.fontSize,
+      fontFamily: typography.fontFamily,
+      color: 'rgba(255,255,255,0.87)',
+      opacity: 1,
+      overflow: 'auto',
+    },
+    action: {
+      float: 'right',
+      lineHeight: '48px',
+      marginRight: -16,
+      backgroundColor: 'transparent',
+      color: 'rgb(255,64,129)',
+    },
+    '@media (min-width: 760px) and (max-width:2000px )': {
+      root: {
+        minWidth: 288,
+        maxWidth: 568,
+        width: 50,
+      },
+    },
+  };
+});
+
+export default function SnackbarContent(props, context) {
+  const {
+    action,
+    contentStyle,
+    message,
+    open, // eslint-disable-line no-unused-vars
+    onActionTouchTap,
+    style,
+    children,
+    rightIconButtonElement,
+    ...other
+  } = this.props;
+
+  const transitionStyle = {
+    transition: open ?
+      transitions.create('opacity', {
+        duration: transitions.duration.complex,
+        delay: transitions.duration.shortest,
+        easing: transitions.easing.easeOut,
+      }) : transitions.create('opacity', {
+        duration: transitions.duration.standard,
+        delay: 0,
+        easing: transitions.easing.easeOut,
+      }),
+  };
+
+  const classes = context.styleManager.render(styleSheet);
+
+  return (
+    <div
+      className={classes.root}
+    >
+      <div className={classes.content} style={{ transitionStyle }}>
+        <span>{message}</span>
+        <div className={classes.action}>
+          {action}
+          {rightIconButtonElement}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+SnackbarContent.propTypes = {
+
+  action: PropTypes.node,
+  /**
+   * Override the inline-styles of the content element.
+   */
+  contentStyle: PropTypes.object,
+  /**
+   * The message to be displayed.
+   *
+   * (Note: If the message is an element or array, and the `Snackbar` may re-render while it is still open,
+   * ensure that the same object remains as the `message` property if you want to avoid the `Snackbar` hiding and
+   * showing again)
+   */
+  message: PropTypes.node,
+  /**
+   * Fired when the action button is touchtapped.
+   *
+   * @param {object} event Action button event.
+   */
+  onActionTouchTap: PropTypes.func,
+  /**
+   * @ignore
+   * Controls whether the `Snackbar` is opened or not.
+   */
+  open: PropTypes.bool,
+  rightElement: PropTypes.node,
+  /**
+   * Override the inline-styles of the root element.
+   */
+  style: PropTypes.object,
+  /**
+   * @ignore
+   * Width of the screen.
+   */
+  width: PropTypes.number,
+};
+
+SnackbarContent.contextTypes = {
+  styleManager: customPropTypes.muiRequired,
+};
